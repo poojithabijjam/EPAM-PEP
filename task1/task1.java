651473024cac8e4d3fca86d82e020be5dcd9c8a0
poojithabijjam <poojitha1589@gmail.com>
2019-04-30 15:39:14 +0530

    Add existing file

---
+++ task1/task1.java
@@ -0,0 +1,18 @@
+package task1;
+
+/**
+ * 
+ * @author poojitha
+
+ * Hello World!  
+ *
+ */
+public class task1 {
+
+	public static void main(String[] args) {
+		
+       System.out.println("Hello World!");
+       
+	}
+
+}
---
+++ task2/simple_compound_interest/Interest.java
@@ -0,0 +1,13 @@
+package task2;
+
+/**
+ * 
+ * @author poojitha
+ * This is an abstract class which can be extended by various classes.
+ * 
+ */
+public abstract class Interest {
+	abstract double interest();
+	double principal,rate_of_interest,years;
+	
+}
